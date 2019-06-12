54c8247a6bd965d749ad2c4c5f95aacdaee9f732
Atal100 <32467670+Atal100@users.noreply.github.com>
2019-06-12 11:51:16 +0200

    Le usermodel extra data toegevoegd

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('../database/mongodb');
 const Schema = mongoose.Schema;
+const image = require('../img/Icon')
 
 const UserSchema = new Schema ({
     username: {
@@ -12,6 +13,19 @@ const UserSchema = new Schema ({
         type: String,
         required: [true, 'Password is required']
     },
+    firstname: {
+        tpye: String,
+        required: [true, 'Firstname is required']
+    },
+    lastname: {
+        type: String,
+        required: [true, 'Lastname is required']
+    },
+    iconurl: {
+        type: String,
+        required: [true, 'Icon is required'],
+        default: image
+    }
 })
 
 const User = mongoose.model('user',UserSchema);
