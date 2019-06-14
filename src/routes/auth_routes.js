d17fd371c06f0683ba4e354a7730f26f30a8c8d3
HarmvDrim <hbddrimm@avans.nl>
2019-06-14 14:04:02 +0200

    added UUID methodes to get and add when you login

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 
 //public routes
 router.post('/login', authController.login);
+router.post('/loginuuid', authController.loginUUID)
 
 module.exports = router;
