6eb4657be6310a89069827d5fdfe13c5e56faf7e
Atal100 <32467670+Atal100@users.noreply.github.com>
2019-06-17 14:04:49 +0200

    Stream merge fixes

--- src/routes/stream_routes.js
+++ src/routes/stream_routes.js
@@ -5,6 +5,7 @@ const streamController = require('../controllers/streamController')
 
 router.get('/streamers', streamController.getStreams)
 router.get('/viewers', streamController.getViewerCount)
+router.post('/stream',streamController.setupInitialStream)
 router.get('/getlivestreams', getLiveStreams)
 
 module.exports = router;
