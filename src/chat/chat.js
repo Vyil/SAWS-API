5c6f90cd059522b6c4da3745b5681b41ec622a5c
Djim <oomesdjim@gmail.com>
2019-06-17 22:15:05 +0200

    First try MessageDB

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -3,18 +3,18 @@ const Streams = require('../models/streams')
 const Message = require('../models/message')
 const ApiError = require('../models/ApiError')
 
-module.exports = (io) =>{
+module.exports = (io) => {
 
     const receivedPath = io.of('/chat')
-    receivedPath.on('connection',(client)=>{
+    receivedPath.on('connection', (client) => {
         //increaseViewer(client.handshake.query.stream)
-        console.log('client connected: '+client.handshake.query.stream)
+        console.log('client connected: ' + client.handshake.query.stream)
         client.join(client.handshake.query.stream)
-        client.on('new-message',(msg)=>{
-            //saveMessageDB(client.handshake.query.stream,msg)
-            receivedPath.to(client.handshake.query.stream).emit('MESSAGE',msg)
+        client.on('new-message', (msg) => {
+            saveMessageDB(client.handshake.query.stream,msg)
+            receivedPath.to(client.handshake.query.stream).emit('MESSAGE', msg)
         })
-        client.on('disconnect',()=>{
+        client.on('disconnect', () => {
             console.log('Client disconnected')
             //decreaseViewer(client.handshake.query.stream)
             client.disconnect()
@@ -22,51 +22,55 @@ module.exports = (io) =>{
         })
     })
 
-    function increaseViewer(stream){
+    function increaseViewer(stream) {
         Streams.findOne(stream)
-            .then((stream)=>{
+            .then((stream) => {
                 viewers = stream.Viewers
                 viewers++;
             })
             .update({
-                Viewers : viewers
+                Viewers: viewers
             })
     }
 
-    function decreaseViewer(stream){
+    function decreaseViewer(stream) {
         Streams.findOne(stream)
-            .then((stream)=>{
+            .then((stream) => {
                 viewers = stream.Viewers
                 viewers--;
                 Streams.update({
-                    Viewers : viewers
+                    Viewers: viewers
                 })
             })
     }
 
-    function saveMessageDB(stream, message){
-        //Add streamID from lookup
-        User.findOne({username:message.username})
-        .then(result=>{
-            newMessage = new Message({
-                content:message.message,
-                stream:stream._id,
-                user:result._id
+    function saveMessageDB(stream, message) {
+
+        Streams.findOne({ username: stream })
+            .then(rslt => {
+                User.findOne({ username: message.username })
+                    .then(result => {
+                        newMessage = new Message({
+                            content: message.message,
+                            stream: rslt._id,
+                            user: result._id
+                        })
+                        newMessage.save()
+                            .then(rt => {
+                                res.status(200).json({ message: 'Message saved succesfully', rt }).end();
+                                return;
+                            })
+                            .catch(err => {
+                                res.status(500).json(new ApiError(err, 500)).end();
+                                return;
+                            })
+                    })
+                    .catch(err => {
+                        res.status(500).json(new ApiError(err, 500)).end();
+                        return;
+                    })
             })
-            newMessage.save()
-            .then(rslt=>{
-                res.status(200).json({message:'Message saved succesfully',rslt}).end();
-                return;
-            })
-            .catch(err=>{
-                res.status(500).json(new ApiError(err,500)).end();
-                return;
-            })
-        })
-        .catch(err=>{
-            res.status(500).json(new ApiError(err,500)).end();
-            return;
-        })
+
     }
 
 }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,72 +10,72 @@ const privateKey = filestream.readFileSync(__dirname + '/cert/private.key');
 
 module.exports = {
 
-    createNewUser(req,res){
+    createNewUser(req, res) {
         console.log('CreateNewUser called ');
 
         User.findOne({
             username: req.body.username
         })
-        .then(result => {
-            if (result) {
-                res.status(409).send(new ApiError('Username already exists', 409)).end();
-            } else {
-                const newUser = new User(req.body, {});
-                newUser.save()
-                    .then(result => {
-                        res.status(200).json({
-                            message: "Created user: " + result
-                        }).end();
-                        return;
-                    })
-                    .catch(err => {
-                        res.status(400).send(new ApiError('Error occurred: ' + err, 400)).end();
-                        return;
-                    });
-            }
-        })
-        .catch(err => {
-            res.status(409).send(new ApiError('Username already exists: ' + err, 409)).end();
-        })
+            .then(result => {
+                if (result) {
+                    res.status(409).send(new ApiError('Username already exists', 409)).end();
+                } else {
+                    const newUser = new User(req.body, {});
+                    newUser.save()
+                        .then(result => {
+                            res.status(200).json({
+                                message: "Created user: " + result
+                            }).end();
+                            return;
+                        })
+                        .catch(err => {
+                            res.status(400).send(new ApiError('Error occurred: ' + err, 400)).end();
+                            return;
+                        });
+                }
+            })
+            .catch(err => {
+                res.status(409).send(new ApiError('Username already exists: ' + err, 409)).end();
+            })
 
     },
 
-    getUserByUUID(req, res){
+    getUserByUUID(req, res) {
         console.log('GetUserByUUID called')
 
         User.findOne({
-            UUID: request.body.UUID
+            uuid: req.body.UUID
         })
-        .then((user)=>{
-            response.status(200).json(user).end()
-        })
-        .catch(error => next(new ApiError(error,500)))
+            .then((user) => {
+                response.status(200).json(user).end()
+            })
+            .catch(error => next(new ApiError(error, 500)))
     },
 
-    getUser(req,res){
-        var queryParam= req.query.username;
+    getUser(req, res) {
+        var queryParam = req.query.username;
 
         //No queryParam = find all
-        if(!queryParam){
+        if (!queryParam) {
             User.find({})
-            .then(rslt=>{
-                res.status(200).json(rslt).end()
-                return;
-            })
-            .catch(err=>{
-                res.status(500).json(new ApiError(err,500)).end()
-                return;
-            })
+                .then(rslt => {
+                    res.status(200).json(rslt).end()
+                    return;
+                })
+                .catch(err => {
+                    res.status(500).json(new ApiError(err, 500)).end()
+                    return;
+                })
         } else {
-        //Query param is find specific
-        User.findOne({username:queryParam})            
-            .then((user)=>{
-                res.status(200).json(user).end()
-                return;
-            })
-            .catch(err=>{
-                res.status(500).json(new ApiError(err,500)).end()
-            })
+            //Query param is find specific
+            User.findOne({ username: queryParam })
+                .then((user) => {
+                    res.status(200).json(user).end()
+                    return;
+                })
+                .catch(err => {
+                    res.status(500).json(new ApiError(err, 500)).end()
+                })
         }
     },
 
@@ -84,10 +84,10 @@ module.exports = {
         try {
             // Request uniform assertions
             const body = request.body;
-            assert(typeof(request.body) === 'object', 'Request body must be of type object');
-            assert(typeof(request.body.payload) === 'object', 'Payload must be of type object');
+            assert(typeof (request.body) === 'object', 'Request body must be of type object');
+            assert(typeof (request.body.payload) === 'object', 'Payload must be of type object');
             assert(body.hash, 'Hash is missing from body');
-            assert(typeof(body.hash) === 'string', 'Hash property must be of type string');
+            assert(typeof (body.hash) === 'string', 'Hash property must be of type string');
 
             // Request specific assertions
             const payload = request.body.payload;
@@ -95,10 +95,10 @@ module.exports = {
             assert(payload.generator, 'Generator is missing from body');
             assert(payload.key, 'Key is missing from body');
 
-            assert(typeof(payload.prime) === 'string', 'Prime property must be of type string');
-            assert(typeof(payload.generator) === 'string', 'Generator property must be of type string');
-            assert(typeof(payload.key) === 'string', 'Key property must be of type string');
-        } catch(error) {
+            assert(typeof (payload.prime) === 'string', 'Prime property must be of type string');
+            assert(typeof (payload.generator) === 'string', 'Generator property must be of type string');
+            assert(typeof (payload.key) === 'string', 'Key property must be of type string');
+        } catch (error) {
             next(new ApiError(error.message, 412));
         }
         console.log('Session ID: ' + request.session.id);
