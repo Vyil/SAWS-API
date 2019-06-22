771024c5ce2fb36732dda0f28fb38e4fea811781
Aron Cornet <acornet@avans.nl>
2019-06-23 01:25:12 +0200

    Added signature checks to the chat messages

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,26 +1,58 @@
-const User = require('../models/user')
-const Streams = require('../models/streams')
-const Message = require('../models/message')
-const ApiError = require('../models/ApiError')
+const User = require('../models/user');
+const Streams = require('../models/streams');
+const Message = require('../models/message');
+const ApiError = require('../models/ApiError');
+const auth = require('../authentication/authentication');
+
+const forge = require('node-forge');
+const pki = forge.pki;
 
 module.exports = (io) => {
-
-    const receivedPath = io.of('/chat')
+    // Set received path origin
+    const receivedPath = io.of('/chat');
+    // Event when a client connects to the socket
     receivedPath.on('connection', (client) => {
-        increaseViewer(client.handshake.query.stream)
-        console.log('client connected: ' + client.handshake.query.stream)
-        client.join(client.handshake.query.stream)
-        client.on('new-message', (msg) => {
-            saveMessageDB(client.handshake.query.stream, msg)
-            receivedPath.to(client.handshake.query.stream).emit('MESSAGE', msg)
-        })
-        client.on('disconnect', () => {
-            console.log('Client disconnected')
-            decreaseViewer(client.handshake.query.stream)
-            client.disconnect()
 
-        })
-    })
+        // Setup variables for later use
+        const query = client.handshake.query;
+        const payload = query.payload;
+        const signature = query.signature;
+
+        // Setting up public key of client for later use
+        const publicKeyPem = payload.publicKey;
+        const publicKey = pki.publicKeyFromPem(publicKeyPem);
+
+        if(auth.verifyDigitalSignature(payload, signature, publicKey)) {
+            // Increase the viewer count and print a console log
+            increaseViewer(payload.stream);
+            console.log('client connected: ' + payload.stream);
+
+            // Join the server on provided stream
+            client.join(payload.stream);
+
+            // Setup event when the client sends a new message
+            client.on('new-message', (msg) => {
+                // Setup variables for later use
+                const newMessagePayload = msg.payload;
+                const newMessageSignature = msg.signature;
+                // Verify the signature, save it to the database and broadcast the message to all listeners with a new signature
+                if(auth.verifyDigitalSignature(newMessagePayload, newMessageSignature, publicKey)) {
+                    saveMessageDB(payload.stream, msg);
+                    receivedPath.to(client.handshake.query.stream).emit('MESSAGE', auth.buildResponse(newMessagePayload))
+                }
+            });
+
+            // Setup event when the client disconnects
+            client.on('disconnect', () => {
+                console.log('Client disconnected');
+                decreaseViewer(payload.stream);
+                client.disconnect()
+            })
+        }
+
+
+
+    });
 
     function increaseViewer(stream) {
         Streams.findOne({username: stream, live: true})
