901cb3f1caa10899a9fc73bd37e097dd1d4d32e3
Aron Cornet <acornet@avans.nl>
2019-06-23 15:38:32 +0200

    Added debug lines

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -20,15 +20,16 @@ module.exports = (io) => {
         const stream = query.stream;
         const username = query.username;
         const certificate = query.certificate;
-        const signature = query.signature.replace(/#/g, '+').replace(/  |\r\n|\n|\r/gm, '') + '==';
+        const signature = query.signature.replace(/[#/%2F]/g, '+').replace(/%20/g,' ').replace(/  |\r\n|\n|\r/gm, '') + '==';
 
         // Construct the payload
         const payload = {
             stream: stream,
             username: username,
-            certificate: certificate.replace(/#/g, '+').replace(/  |\r\n|\n|\r/gm, '')
+            certificate: certificate.replace(/[#/%2F]/g, '+').replace(/%20/g,' ')
         };
-
+        console.log('Signature: ' + signature);
+        console.log('Certificate: ' + certificate.replace(/[#/%2F]/g, '+').replace(/%20/g,' '));
         // Find the current certificate in the database
         Certificate.findOne({
             username: payload.username,
